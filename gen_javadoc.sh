71f182bb1a5141b1c73c34ac6e0286e7758de792
muquit <muquit@gmail.com>
2017-09-10 15:14:26 -0400

    Add javadocs link

--- gen_javadoc.sh
+++ gen_javadoc.sh
@@ -3,7 +3,9 @@
 #javadoc -d ~/x -stylesheetfile ~/Downloads/java/javadoc_css/stylesheet.css  $(find . -name *.java)
 /bin/rm -rf ./target/docs/*
 javadoc -d ./target/docs  $(find . -name *.java)
-#cp -av ./target/docs/* ../gh-pages/javadoc-test/
+
+cp -av ./target/docs/* ../gh-pages/libsodium-jna/
+
 hdir="/var/www/html/libsodium-jna"
 mkdir -p ${hdir}
 /bin/rm -rf ${hdir}/*
